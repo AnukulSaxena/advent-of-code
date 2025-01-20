6f3e1569c5699b2085b35e5fec76db80c3519a60
Anukul Prakash Saxena <101214911+AnukulSaxena@users.noreply.github.com>
2025-01-20 13:47:36 +0000

    changes

--- 2024/Day-2/puzzle-2.js
+++ 2024/Day-2/puzzle-2.js
@@ -19,8 +19,37 @@ getReports().then((reports) => {
         let isIncreasing = report[0] < report[1];
         for (let i = 1; i < report.length; i++) {
             const difference = Math.abs(report[i] - report[i - 1]);
-            if (report[i - 1] < report[i] !== isIncreasing) {
+            let some = [];
 
+            if (report[i - 1] < report[i] !== isIncreasing) {
+            console.log(report)
+                
+                if(report[i-1] < report[i]){
+                    some = report.filter((_,index) => index !== i)
+                }
+                else{
+                   some = report.filter((_,index) => index !== i-1)
+                }
+                console.log( some);
+                return true;
+            }
+
+            if(report[i - 1] === report[i]){
+            console.log(report)
+
+                some = report.filter((_,index) => index !== i)
+                console.log( some);
+                return true;
+            }
+
+            if(difference > 3){
+            console.log(report)
+
+                some = report.filter((_,index) => index !== i)
+                console.log(some)
+                some = report.filter((_,index) => index !== i-1)
+                console.log( some);
+                return true;
             }
 
         }
